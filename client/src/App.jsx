c75a59230e7e6d9f0c0873bf1a797e11d8e4b702
lewiskemboi <Lewis.Kemboi@griffinglobaltech.com>
2024-08-28 12:37:47 +0300

    -Correct access interception.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,11 @@
-import React, { useEffect } from 'react'
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import AuthRoutes from './pages/auth/AuthRoutes.jsx'
 import Dashboard from './layout/Dashboard.jsx'
-import { clearStorageOnTokenExpiry } from './helpers/token.js'
 
 function App() {
-  const logout = useNavigate();
   
-  useEffect(() => {
-    const response = clearStorageOnTokenExpiry();
-
-    if (response && response.route) {
-      logout(response.route, { replace: true });
-    }
-  }, [logout]);
-
   return (
     <>
       <Routes>
